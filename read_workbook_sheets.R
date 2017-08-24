07c8e498ab2f623e97dc9cfd29b72812e2ba8f8d
Jane Bond <estearns88@gmail.com>
2017-08-24 15:04:10 -0700

    transformed into function

--- read_workbook_sheets.R
+++ read_workbook_sheets.R
@@ -14,15 +14,20 @@ workbook_name <- 'test.xlsx'
 
 # ----------------------------------------------- set up environment -------------------------------------------
 .libPaths(my_lib)
-pacman::p_load(data.table, dplyr,plyr,XLConnect)
-# ----------------------------------------------- load data -------------------------------------------
-pizza <- loadWorkbook(paste0(data_dir, workbook_name))
+pacman::p_load(XLConnect)
 
-sheet_names <- getSheets(pizza)
-names(sheet_names) <- sheet_names
+# ----------------------------------------------- Function -----------------------------------------------------
+load_sheets <- function(workbook){
+  pizza <- loadWorkbook(workbook)
+  
+  sheet_names <- getSheets(pizza)
+  names(sheet_names) <- sheet_names
+  
+  sheet_list <- lapply(sheet_names, function(.sheet){
+      readWorksheet(object=pizza, .sheet)})
+  
+  return(sheet_list)
+}
 
-sheet_list <- lapply(sheet_names, function(.sheet){
-  readWorksheet(object=pizza, .sheet)})
-
-
-
+# ----------------------------------------------- Function call -----------------------------------------------------
+allsheets <- load_sheets(paste0(data_dir, workbook_name))
